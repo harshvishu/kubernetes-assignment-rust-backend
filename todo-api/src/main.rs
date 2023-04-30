02be80cf3c94276a5859983b8014a74e51fa4935
harsh vishwakarma <vishwakarma.harsh1993@gmail.com>
2023-04-30 22:49:35 +0530

    initial commit

---
+++ todo-api/src/main.rs
@@ -0,0 +1,39 @@
+#[macro_use]
+extern crate rocket;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct TodoItem {
+    id: u32,
+    text: String,
+}
+
+#[get("/")]
+fn get_items() -> String {
+    let items = vec![
+        TodoItem {
+            id: 1,
+            text: String::from("Buy milk"),
+        },
+        TodoItem {
+            id: 2,
+            text: String::from("Do laundry"),
+        },
+        TodoItem {
+            id: 3,
+            text: String::from("Clean room"),
+        },
+    ];
+
+    serde_json::to_string(&items).unwrap()
+}
+
+#[launch]
+fn rocket() -> _ {
+    let figment = rocket::Config::figment()
+        .merge(("port", 8080))
+        .merge(("address", "0.0.0.0"));
+
+    rocket::custom(figment).mount("/", routes![get_items])
+}
