a3a2db078e09171976e9910efaaf7f281d546f62
harsh vishwakarma <vishwakarma.harsh1993@gmail.com>
2023-06-16 13:20:41 +0530

    updated sql seed data

--- todo-migration/seed.sql
+++ todo-migration/seed.sql
@@ -1,4 +1,6 @@
 -- Insert sample data into the 'todos' table
-INSERT INTO todos (body, completed) VALUES ('Task 1', FALSE);
-INSERT INTO todos (body, completed) VALUES ('Task 2', TRUE);
-INSERT INTO todos (body, completed) VALUES ('Task 3', FALSE);
+INSERT INTO todos (body, completed) VALUES ('ShutDown System when not needed', FALSE);
+INSERT INTO todos (body, completed) VALUES ('Update OS', TRUE);
+INSERT INTO todos (body, completed) VALUES ('Complete assignment before 20', FALSE);
+INSERT INTO todos (body, completed) VALUES ('Buy Milk', FALSE);
+INSERT INTO todos (body, completed) VALUES ('Pay Bills', FALSE);
